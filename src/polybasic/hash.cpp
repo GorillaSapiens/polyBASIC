87848ecb62e2a27c127089b4658a5fd34dd76892
GorillaSapiens <embracetheape@gmail.com>
2023-02-21 23:30:53 -0500

    added hashing functions

---
+++ src/polybasic/hash.cpp
@@ -0,0 +1,11 @@
+static unsigned long hash(const char *sstr) {
+   const unsigned char *str = (const unsigned char *)sstr;
+   unsigned long hash = 5381;
+   int c;
+
+   while (c = *str++)
+      hash = ((hash << 5) + hash) ^ c; // hash(i - 1) * 33 ^ str[i]
+
+   return hash;
+}
+
