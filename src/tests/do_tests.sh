6dc0fcc36cc52a13f626dc3d1120a536e8051bae
GorillaSapiens <embracetheape@gmail.com>
2023-02-24 18:24:26 -0500

    updated tests

--- src/tests/do_tests.sh
+++ src/tests/do_tests.sh
@@ -1,3 +1,9 @@
 #!/bin/sh
 
-POLYBASICLANG=reverse POLYBASICPATH=../translations ../polybasic/polybasic tests.txt
+export POLYBASICPATH=../translations
+echo ================== english test
+../polybasic/polybasic -l english english.bas
+echo ================== reverse test
+../polybasic/polybasic -l reverse reverse.bas
+echo ================== emoji test
+../polybasic/polybasic -l emoji emoji.bas
