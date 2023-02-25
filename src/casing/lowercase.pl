bbb2fd47ebb0e3ba3d31a1b392d625f37334579c
GorillaSapiens <embracetheape@gmail.com>
2023-02-24 20:27:07 -0500

    adjustments for unicode madness

--- src/casing/lowercase.pl
+++ src/casing/lowercase.pl
@@ -16,6 +16,10 @@ printf("} LCMAP;\n");
 printf("\n");
 printf("LCMAP lcmap[] = {\n");
 
+%map = ();
+
+# first, the plain old mappings....
+
 open FILE, "<UnicodeData.txt";
 while (<FILE>) {
    s/[\x0a\x0d]//g;
@@ -28,6 +32,55 @@ while (<FILE>) {
       $codevalue = chr(hex($codevalue));
       $lowercase = chr(hex($lowercase));
       print "   { \"$codevalue\", \"$lowercase\" },\n";
+   }
+}
+close FILE;
+
+# second, the complicated stuff...
+sub recode($) {
+   my $arg = shift @_;
+   if (!length($arg)) {
+      return "";
+   }
+   my @x = split / /, $arg;
+   foreach my $x (@x) {
+      $x = chr(hex($x));
+   }
+   return join("", @x);
+}
+
+open FILE, "<SpecialCasing.txt";
+while (<FILE>) {
+   s/[\x0a\x0d]//g;
+   s/^#.*//g;
+   if (length($_)) {
+      @x = split /;/,$_;
+      $code = shift @x;
+
+      $lower = shift @x;
+      $lower =~ s/^[ ]*//g;
+      $lower =~ s/[ ]*$//g;
+
+      $title = shift @x;
+      $title =~ s/^[ ]*//g;
+      $title =~ s/[ ]*$//g;
+
+      $upper = shift @x;
+      $upper =~ s/^[ ]*//g;
+      $upper =~ s/[ ]*$//g;
+
+      if (length($lower)) {
+         $lower = recode($lower);
+         $title = recode($title);
+         $upper = recode($upper);
+
+         if (length($title) && length($lower)) {
+            print "   { \"$title\", \"$lower\" },\n";
+         }
+         if (length($upper) && length($lower)) {
+            print "   { \"$upper\", \"$lower\" },\n";
+         }
+      }
    }
 }
 close FILE;
