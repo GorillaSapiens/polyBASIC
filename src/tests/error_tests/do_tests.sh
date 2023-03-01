82e70fef8921d80678e3cf248329cf80ea8a7658
GorillaSapiens <embracetheape@gmail.com>
2023-02-28 22:29:26 -0500

    first error test

---
+++ src/tests/error_tests/do_tests.sh
@@ -0,0 +1,21 @@
+#!/bin/sh
+
+export PATH=$PATH:../../polybasic
+export POLYBASICPATH=../../translations
+
+for file in `ls *.bas`
+do
+   base=`basename -s .bas $file`
+   echo ===== $base
+   polybasic -l english $base.bas 2>&1 > $base\_actual.txt
+   DIFF=$(diff -q $base\_expected.txt $base\_actual.txt)
+   if [ "$DIFF" ]
+   then
+      echo FAIL
+      exit 1
+   else
+      echo PASS
+   fi
+done
+
+exit 0
