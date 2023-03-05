eec5ea25de30d2ee066f07d2435b7fdff1000fcb
GorillaSapiens <embracetheape@gmail.com>
2023-03-05 14:05:51 -0500

    added missing include

---
+++ src/polybasic/main.hpp
@@ -0,0 +1,8 @@
+#ifndef _INCLUDE_MAIN_HPP_
+#define _INCLUDE_MAIN_HPP_
+
+extern const char *has_tuple(const char *token);
+extern const char *eop2string(int op);
+extern bool void_enabled;
+
+#endif
